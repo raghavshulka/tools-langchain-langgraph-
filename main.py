ba2d3b3a2a8b320637a3d16564be6119c4faaf0f
raghavshulka <himanshu4shukla4l@gmail.com>
2025-09-25 22:22:07 +0530

    done

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
+import array
+from typing import List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 app = FastAPI()
+
+class todoType(BaseModel):
+    topic: str
+    todo: str
+    status: str
+
+
+    @field_validator('topic')
+    @classmethod
+    def uppercaseTopic(cls,value):
+        return value.upper()
+
+
+arr = []
 
 @app.get("/")
 def root():
     return {"hello from server"}
 
 
+@app.get("/todos", response_model=List[todoType])
+def get_todos():
+    return arr
+
+
+@app.post("/set")
+def set_todos(data:todoType):
+    arr.append(data)
+    return data 
 
 
 
+@app.delete("/delete/{title}")
+def delete_todo(title: str):
+    for i, todo in enumerate(arr):
+        if todo.topic == title:
+            print(todo,title)
+            removed = arr.pop(i)
+            return {"message": f"Deleted todo '{removed}'"}
     
 
 
