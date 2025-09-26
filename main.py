4b5fd5354ed0b2b992f403da9e52e6bb74e10642
raghavshulka <himanshu4shukla4l@gmail.com>
2025-09-26 12:31:43 +0530

    parallel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from langchain_core.runnables import RunnableParallel
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -10,21 +11,35 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.8)
 
 
+
 prompt = PromptTemplate(
     template='tell me about a{topic}',
     input_variables=['topic']
 )
 
+prompt1 = PromptTemplate(
+    template='Generate 5 short question  from the following topic \n {topic}',
+    input_variables=['topic']
+)
+
+prompt3 = PromptTemplate(
+    template='Merge the provided notes and quiz into a single document \n notes -> {essay} and quiz -> {points}',
+    input_variables=['essay', 'points']
+)
+
 
 parser = StrOutputParser()
 
+parallel = RunnableParallel({
+    'essay': prompt | llm | parser ,
+    'points': prompt1 | llm | parser
+})
 
-chain = prompt | llm | parser
+mergeChain = prompt3 | llm | parser
 
+chain = parallel | mergeChain
 
-result = chain.invoke({'topic':'python language'})
+result = chain.invoke({'topic':'what is quantum-computing'})
 
 print(result)
-
-
 chain.get_graph().print_ascii()
