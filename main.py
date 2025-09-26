945cf82d536fa4589192d0d97c53d3493fc99774
raghavshulka <himanshu4shukla4l@gmail.com>
2025-09-26 13:08:13 +0530

    if-else-branch

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
+from typing import Literal
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableBranch, RunnableLambda
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
+from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
+class Feedback(BaseModel):
+    sentiment: Literal['advantage', 'disadvantage'] = Field(description="Classify as advantage or disadvantage")
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.8)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.8)
 
+parser2= PydanticOutputParser(pydantic_object=Feedback)
 
-
-prompt = PromptTemplate(
-    template='tell me about a{topic}',
-    input_variables=['topic']
+classifier_prompt = PromptTemplate(
+    template="Classify the following text as either advantage or disadvantage:\n\n{topic}\n{format_instruction}",
+    input_variables=["topic"],
+    partial_variables={"format_instruction": parser2.get_format_instructions()}
 )
 
-prompt1 = PromptTemplate(
-    template='Generate 5 short question  from the following topic \n {topic}',
-    input_variables=['topic']
+classifier_chain = classifier_prompt | llm | parser2 
+
+advantage_prompt = PromptTemplate(
+    template="Write advantages of {topic}",
+    input_variables=["topic"]
 )
 
-prompt3 = PromptTemplate(
-    template='Merge the provided notes and quiz into a single document \n notes -> {essay} and quiz -> {points}',
-    input_variables=['essay', 'points']
+disadvantage_prompt = PromptTemplate(
+    template="Write disadvantages of {topic}",
+    input_variables=["topic"]
 )
-
 
 parser = StrOutputParser()
 
-parallel = RunnableParallel({
-    'essay': prompt | llm | parser ,
-    'points': prompt1 | llm | parser
-})
+branch_chain = RunnableBranch(
+    (lambda x: x.sentiment == "advantage", advantage_prompt | llm | parser),
+    (lambda x: x.sentiment == "disadvantage", disadvantage_prompt | llm | parser),
+    RunnableLambda(lambda _: "Could not classify sentiment.")
+)
 
-mergeChain = prompt3 | llm | parser
+chain = classifier_chain | branch_chain
 
-chain = parallel | mergeChain
-
-result = chain.invoke({'topic':'what is quantum-computing'})
-
+result = chain.invoke({"topic": "what is advantage quantum-computing"})
 print(result)
+
 chain.get_graph().print_ascii()
